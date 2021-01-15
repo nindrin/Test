75d7a726d9f7c6361800ac8d6d47e01ffdfeefbd
nindrin <719941628@qq.com>
2021-01-15 13:55:50 +0800

    Delete BBACC.sh

--- BBACC.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
-export PATH
-
-[[! -e bug]] && exit 1
-if ! wget --no-check-certificate https://raw.githubusercontent.com/nindrin/Test/main/BBACC.zip; then
-			echo -e "BBACC.zip 下载失败 !" && exit 1
-fi 
-unzip -P  "BBACC.zip" && chmod +x ssr.sh && bash ssr.sh
